b0d1f1d0f1318036001b377a45816a1be5e35693
Benjamin Mantle <BadDuke@users.noreply.github.com>
2020-01-27 18:38:37 -0500

    Fixed spacing and a comment that bothered me at a glance

--- bouncer.js
+++ bouncer.js
@@ -3,8 +3,8 @@
 function bouncer(arr) {
 
 	// pass in Boolean and invoke its ctor as callback function
-	return arr.filter(Boolean); // sreturns an array with falsy elements removed
+	return arr.filter(Boolean); // returns an array with falsy elements removed
 }
 
-bouncer([1,2,"b",0,{},"",NaN,3,undefined,null,5]);
-// [1,2,"b",{},3,5]
+bouncer([1, 2, "b", 0, {}, "", NaN, 3, undefined, null, 5]);
+// [1, 2, "b", {}, 3, 5]
